37760dd3504dc7692275427db9462650beded2d4
nimish07-ai <nimishboda7@gmail.com>
2023-03-25 13:18:42 +0530

    inital commit

---
+++ index.js
@@ -0,0 +1,50 @@
+const toggle = () => {
+    const nav = document.getElementById("topnav");
+    nav.className === "topnav" ? nav.className += " responsive" : nav.className = "topnav";
+};
+
+
+
+
+content={
+    First_year:[
+   
+        {header:"linear algebra",
+        picture:"pic",
+        content:"aidad",
+        },
+        {header:"linear algebra",
+        picture:"pic",
+        content:"aidad",
+        },
+        {header:"linear algebra",
+        picture:"pic",
+        content:"aidad",
+        },
+   
+    ],
+    Second_year:[
+   
+        {header:"",
+        picture:"",
+        content:"",
+        }
+   
+    ],
+    Third_year:[
+   
+        {header:"",
+        picture:"",
+        content:"",
+        }
+   
+    ],
+    Forth_year:[
+   
+        {header:"",
+        picture:"",
+        content:"",
+        }
+   
+    ]
+}
